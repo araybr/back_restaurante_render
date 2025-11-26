316fe0170b290308756c1bcf20dc82e8d9320403
araybr <araybrito2005@gmail.com>
2025-11-26 19:22:40 +0100

    Cambios en Pedido para eliminar detalles de pedido

--- BACK/restaurante/src/main/java/com/restaurante/repository/AlergenoRepository.java
+++ BACK/restaurante/src/main/java/com/restaurante/repository/AlergenoRepository.java
@@ -4,5 +4,8 @@ package com.restaurante.repository;
 import com.restaurante.model.Alergeno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AlergenoRepository extends JpaRepository<Alergeno, Integer> {
+    Optional<Alergeno> findByNombre(String nombre);
 }
