9200c1c489eeed126547bfe590f256057e8a88cb
araybr <araybrito2005@gmail.com>
2025-12-04 19:57:53 +0100

    Ultimos camios para desplegarlo en render sin fallos, el cors para habilitar netlify y el .jar para compilar todo el proyecto.

--- BACK/restaurante/src/main/java/com/restaurante/config/CorsConfig.java
+++ BACK/restaurante/src/main/java/com/restaurante/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://cerulean-malasada-07a2af.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
